9591c077f5f951bc33a051d7066767a25e97bcc6
David <davcastellanoslarios@gmail.com>
2021-11-06 15:06:52 -0600

    arrow funct

---
+++ 02-Fundamentos/flecha.js
@@ -0,0 +1,13 @@
+
+// function sumer(a,b){
+    // return a + b
+// }
+
+ const sumer = (a,b = 33) => {
+     return a + b
+ }
+
+ const saludar = () => 'hola mundo'
+
+ console.log(saludar());
+console.log( sumer(5) ) // 38
