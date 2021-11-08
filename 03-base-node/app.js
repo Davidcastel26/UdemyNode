7fa0f0b962e3898a6ff3a9bf6f17fbbb05934208
David <davcastellanoslarios@gmail.com>
2021-11-08 11:57:35 -0600

    adding dependencies

--- 03-base-node/app.js
+++ 03-base-node/app.js
@@ -1,26 +1,37 @@
 // print 
 // const fs = require('fs');//fs = file system
 const { crearTabla } = require('./helpers/multiplicar')
+const argv = require('yargs').argv;
+
 
 console.clear()
+
+// ----------- CONSOLE LOG FROM THE TOTAL BT -----------
 // console.log(`tabla del ${t}`)
 
+
+// ------ CONSOLE LOG FRON THE ARGV ----------------------
 // console.log(process.argv); //thge console will give us this array[
 //'C:\\Program Files\\nodejs\\node.exe',
 //'C:\\Users\\ingac\\Documents\\progra\\UdemyNode\\03-base-node\\app',
 //'--base=9'
 // ]
 
-const [,,argument3= 'base=5'] = process.argv
-const [,base = 1] = argument3.split('=')
+//-------------- TAKING THE ARGV FRON THE NODE FS --------------
+// const [,,argument3= 'base=5'] = process.argv
+// const [,base = 1] = argument3.split('=')
 // console.log(argument3); //. --base=1
 // console.log(base); // 1
 // let t = 4;
 
-crearTabla(base)
-  .then( nombreArchi => console.log(nombreArchi, 'creado'))
-  .catch(err => console.log(err))
+//---------- CREATE TABLE -----------------------
+// crearTabla(base)
+  // .then( nombreArchi => console.log(nombreArchi, 'creado'))
+  // .catch(err => console.log(err))
 
+
+
+// ---------- PROMISE ----------------------------
 // console.log(salida);
   // fs.writeFile( `tabla-${t}.txt`, salida,(err)=>{
   //     if(err) throw err
