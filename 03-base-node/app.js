fe7ebd863a61e1ea4e2ac8963abc9a2a9b423f84
David <davcastellanoslarios@gmail.com>
2021-11-07 20:22:40 -0600

    tabla update xd

--- 03-base-node/app.js
+++ 03-base-node/app.js
@@ -1,6 +1,10 @@
 // print 
-
 let t = 5
+
+console.clear()
+console.log('------------');
+console.log(`tabla del ${t}`)
+console.log('------------');
 
 for(let i = 0; i <= 10; i++ ){
   let total = i * t;
