61df0d4555d3a088c5a0e3a82861b1d5f3f7e30f
David Castellanos <davcastellanoslarios@gmail.com>
2023-05-17 18:20:18 -0600

    working on server.js

--- restServer/models/server.js
+++ restServer/models/server.js
@@ -5,12 +5,22 @@ class Server {
     constructor(){
         this.app = express()
         this.port = process.env.PORT
+        
+        // middlewares 
+        this.middlewares()
 
+        // APP ROUTES 
         this.routes();
     }
 
+    middlewares(){
+        // public dir
+        this.app.use( express.static('public'))
+
+    }
+
     routes(){
-        this.app.get('/', (req, res) => {
+        this.app.get('/api', (req, res) => {
             res.send('Hello World!')
         })
     }
