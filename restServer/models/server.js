1aa440d2af2f86ec0f8ef0f4e7991f87c66d879d
David Castellanos <davcastellanoslarios@gmail.com>
2023-05-17 18:43:03 -0600

    working on server

--- restServer/models/server.js
+++ restServer/models/server.js
@@ -21,7 +21,24 @@ class Server {
 
     routes(){
         this.app.get('/api', (req, res) => {
-            res.send('Hello World!')
+            res.status(200).json({
+                "msg":"get API"
+            })
+        })
+        this.app.put('/api', (req, res) => {
+            res.status(202).json({
+                "msg":"Update API"
+            })
+        })
+        this.app.post('/api', (req, res) => {
+            res.status(201).json({
+                "msg":"Create API"
+            })
+        })
+        this.app.delete('/api', (req, res) => {
+            res.status(200).json({
+                "msg":"Delete API"
+            })
         })
     }
 
