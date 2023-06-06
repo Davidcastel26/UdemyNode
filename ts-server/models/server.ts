2b22b32747e2f677dd83fd097127a2f92a88ff3b
David Castellanos <davcastellanoslarios@gmail.com>
2023-06-05 18:47:11 -0600

    working on env dot

--- ts-server/models/server.ts
+++ ts-server/models/server.ts
@@ -3,9 +3,11 @@ import express, { Application } from 'express';
 class Server{
 
     private app: Application;
+    private port: string;
 
     constructor(){
         this.app = express();
+        this.port = process.env.PORT || '8000';
     }
 
 }
