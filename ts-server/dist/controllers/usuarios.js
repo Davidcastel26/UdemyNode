f985e5855587ebf036adc6d268f3a5b181f0dc60
David Castellanos <davcastellanoslarios@gmail.com>
2023-06-08 10:37:58 -0600

    working on prisma and sql

--- ts-server/dist/controllers/usuarios.js
+++ ts-server/dist/controllers/usuarios.js
@@ -1,11 +1,29 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteUser = exports.updateUser = exports.postUser = exports.getUser = exports.getUsers = void 0;
-const getUsers = (req, res) => {
-    res.json({
-        msg: 'GetUusuarios'
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.deleteUser = exports.updateUser = exports.postUser = exports.getUser = exports.getUsers = void 0;
+const client_1 = require("@prisma/client");
+const { user } = new client_1.PrismaClient();
+const getUsers = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const users = yield user.findMany({
+        select: {
+            name: true,
+            email: true
+        }
+    });
+    res.json({
+        msg: 'GetUusuarios',
+        users
+    });
+});
 exports.getUsers = getUsers;
 const getUser = (req, res) => {
     const { id } = req.params;
