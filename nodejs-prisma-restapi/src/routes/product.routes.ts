09d6534fdae398fa22255b7f2f59cbc9b0771fbf
David Castellanos <davcastellanoslarios@gmail.com>
2023-06-09 13:33:46 -0600

    working on routes

--- nodejs-prisma-restapi/src/routes/product.routes.ts
+++ nodejs-prisma-restapi/src/routes/product.routes.ts
@@ -1,10 +1,12 @@
 import { getProducts,   
-         getProduct } from "../controllers/product";
+         getProduct, 
+         postProduct} from "../controllers/product";
 import { Router } from "express";
 
 const router = Router();
 
 router.get('/',getProducts)
 router.get('/:id', getProduct)
+router.post('/', postProduct)
 
 export default router;
