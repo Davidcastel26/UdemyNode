6a78fc8ee2313d53f904b9540a78f04aae47f71f
David Castellanos <davcastellanoslarios@gmail.com>
2023-08-17 19:05:30 -0600

    web sockets working

--- websocket-nodejs-crud/src/index.js
+++ websocket-nodejs-crud/src/index.js
@@ -1,1 +1,16 @@
-console.log('hello word')
+import express from "express";
+import { Server as SocketServer } from 'socket.io'
+import http from 'http'
+
+const app = express()
+const server = http.createServer(app)
+const io = new SocketServer(server)
+
+app.use(express.static(__dirname+ '/public'))
+
+io.on('connection', (socket) => {
+    console.log('nueva connection: ', socket.id);
+})
+
+server.listen(8000)
+console.log('server on port', 8000);
